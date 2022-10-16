9bee9a2b229efa07f4b76c364d04dcb72d14eefe
Bhanuka <bhanukamax@gmail.com>
2022-10-16 19:07:44 +0530

    printing key value on keypress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rdev::{listen, Event, EventType};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world agaaaain!");
+
+    if let Err(error) = listen(callback) {
+        println!("Error: {:?}", error)
+    }
+}
+
+fn callback(event: Event) {
+     if let EventType::KeyPress(key) = event.event_type {
+        println!("this is the key press {:?}", key);
+    }
 }
